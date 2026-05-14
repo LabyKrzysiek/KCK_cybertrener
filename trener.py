c22d2b232acd917a0dd50018b9c0ce96d4e47780
JakubBetlej <255564@edu.p.lodz.pl>
2026-05-14 13:37:28 +0200

    Dodanie wykrywania pozycji oraz prostego gui

--- trener.py
+++ trener.py
@@ -18,6 +18,9 @@ pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 cap = cv2.VideoCapture(0)
 print("Kamera uruchomiona. Wciśnij 'q', aby zamknąć okno.")
 
+counter = 0
+stage = "BRAK"
+
 while cap.isOpened():
     success, frame = cap.read()
     if not success:
@@ -25,7 +28,6 @@ while cap.isOpened():
         break
 
     h, w, _ = frame.shape
-
     image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = pose.process(image_rgb)
 
@@ -51,21 +53,40 @@ while cap.isOpened():
             hip_angle = calculate_angle(shoulder, hip, knee)
             knee_angle = calculate_angle(hip, knee, ankle)
 
+            if hip_angle < 110 and knee_angle < 120:
+                stage = "STARTOWA"
+                
+            elif stage == "STARTOWA" and (hip_angle >= 110 or knee_angle >= 120):
+                stage = "PODNOSZENIE"
+                
+            elif hip_angle > 160 and knee_angle > 160:
+                if stage == "PODNOSZENIE":
+                    counter += 1
+                stage = "PELNY WYPROST"
+                
+            elif stage == "PELNY WYPROST" and (hip_angle < 160 or knee_angle < 160):
+                stage = "OPUSZCZANIE"
+
             hip_pixel_pos = (int(hip[0] * w), int(hip[1] * h))
             knee_pixel_pos = (int(knee[0] * w), int(knee[1] * h))
 
-            cv2.putText(frame, str(int(hip_angle)), 
-                        hip_pixel_pos, 
+            cv2.putText(frame, str(int(hip_angle)), hip_pixel_pos, 
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            
-            cv2.putText(frame, str(int(knee_angle)), 
-                        knee_pixel_pos, 
+            cv2.putText(frame, str(int(knee_angle)), knee_pixel_pos, 
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         except Exception as e:
             pass
 
-    cv2.imshow('Personal Trainer - Deadlift Angles', frame)
+    cv2.rectangle(frame, (0, 0), (320, 110), (245, 117, 16), -1)
+    
+    cv2.putText(frame, 'POWT:', (15, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, str(counter), (110, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
+    
+    cv2.putText(frame, 'FAZA:', (15, 85), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+    cv2.putText(frame, stage, (110, 90), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2, cv2.LINE_AA)
+
+    cv2.imshow('cybertrener', frame)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
